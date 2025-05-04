2c3b9e8f0b44dae58ac5400911cb18359a478bd0
Arruda <felipearrudadasilva58@gmail.com>
2025-05-04 10:46:35 -0300

    up

--- Front-end/Caps js/cap2.js
+++ Front-end/Caps js/cap2.js
@@ -8,7 +8,124 @@ document.addEventListener('DOMContentLoaded', function() {
   
   // Conteúdo do capítulo
   const chapterContent = `
-    
+
+<p>Depois de quem sabe quanto tempo, Fernando começou lentamente a recuperar a consciência.</p>  
+
+<p>Seu corpo parecia estar tão pesado quanto metal, os músculos doíam e uma leve sensação entorpecente envolvia seus membros. Suas pálpebras estavam muito pesadas, mesmo assim, tentou ao máximo forçá-las a abrir, mas era inútil.</p>  
+
+<p>O que aconteceu comigo? O que eram aquelas luzes assustadoras?</p>  
+
+<p>Após acordar, ele continuamente se recordou daquela situação aterrorizante que passou. O medo estava enraizado profundamente em seu ser.</p>  
+
+<p>E se ele já estivesse morto? Então essa era a vida pós-morte? Esse pensamento passou por sua mente, mas ao sentir a dor nos músculos em todo seu corpo, rapidamente rejeitou essa ideia.</p>  
+
+<p>Depois de alguns minutos naquele estado, sentiu que a sensação de entorpecimento e peso estava lentamente desaparecendo.</p>  
+
+<p>Com algum esforço, finalmente conseguiu mover seu dedo indicador da mão direita, depois o polegar e, finalmente, toda a mão. Ele então pensou em tentar abrir os olhos.</p>  
+
+<p>Fernando focou em suas pálpebras, até lentamente conseguir abri-las. Assim que uma pequena brecha foi aberta, raios de luz encheram suas pupilas, forçando-o a fechar os olhos novamente.</p>  
+
+<p>O que está acontecendo??! Me recordo claramente de estar deitado na minha cama! Talvez aquelas luzes estranhas tenham me abduzido. Será que os alienígenas realmente existem e me pegaram???? Por acaso vou virar uma daquelas pessoas de relatos na TV que se tornaram cobaias?? Droga! Até os extraterrestres vem aqui na Terra me ferrar? Será que nasci tão azarado assim?</p>  
+
+<p>Diversos pensamentos criativos inundaram sua mente.</p>  
+
+<p>Depois de alguns segundos, finalmente conseguiu se acalmar, então tentou abrir os olhos novamente. A luz era extremamente forte, mas aos poucos foi se acostumando, porém, ao contrário das suas conjecturas iniciais, não se tratava de luzes artificiais, mas luz solar! Fernando aparentemente estava em um local ao ar livre.</p>  
+
+<p>Ao se acostumar mais com a claridade, tentou averiguar seus arredores pelos cantos dos olhos. Para sua surpresa, parecia haver outras pessoas deitadas no chão em torno dele!!</p>  
+
+<p>Quem são essas pessoas? Talvez elas também tenham sido abduzidas.</p>  
+
+<p>Logo chegou a uma conclusão e uma sensação de alívio percorreu sua mente. Pelo menos ele não estava sozinho nessa furada, havia mais gente!</p>  
+
+<p>O ser humano era assim. Ao enfrentar situações desconhecidas, é comum ficar assustado, mas ao enfrentar isso na companhia de outros, o medo diminui consideravelmente.</p>  
+
+<p>É como as pessoas que têm medo do escuro, apesar de ser assustador, quando tem alguém com elas, não se torna mais tão amedrontador.</p>  
+
+<p>Fernando tentou mover a cabeça para o lado esquerdo e, graças ao fato do entorpecimento estar enfraquecendo, realmente conseguiu.</p>  
+
+<p>Ao observar a situação no lado esquerdo, teve uma surpresa. Não muito longe, a cerca de um metro e meio, tinha uma bela garota ruiva o encarando!</p>  
+
+<p>Aparentemente, ela também estava paralisada. Deitada de lado, tudo que conseguia fazer era abrir e fechar os olhos e continuar observando. Fernando instantaneamente se sentiu nervoso e levemente intimidado, não só pela situação estranha, como pelo fato dessa garota ser extremamente bonita!</p>  
+
+<p>Ela parecia ter em torno de vinte anos. Tinha uma pele clara e olhos verdes brilhantes e estava vestida com uma camisa amarela e calça jeans apertada.</p>  
+
+<p>Ele certamente não sabia como reagir, ser encarado tão de perto por essa bela moça era sufocante!</p>  
+
+<p>Ambos se observaram por alguns segundos, até que Fernando não suportou mais essa situação estranha e vexatória, logo tentou focar em seu próprio corpo, movimentando outras partes. Ele praticamente já tinha controle da mão direita, então focou no braço direito.</p>  
+
+<p>Aos poucos, foi ganhando controle e outras partes do corpo também foram se libertando da paralisia. Logo, conseguiu movimentar seu torso e braço esquerdo, então tentou se levantar devagar e finalmente conseguiu se sentar.</p>  
+
+<p>Após se posicionar sentado, Fernando pôde observar mais longe. Foi nesse momento que percebeu que havia pelo menos centenas de pessoas deitadas, quase amontoadas em sua volta, com apenas um pequeno espaço entre elas. De repente, ouviu uma voz distante.</p>  
+
+<p>“Olha só, Capitão, parece que um dos recrutas acordou!”</p>  
+
+<p>“Vá verificar o estado dele!”</p>  
+
+<p>Ele rapidamente olhou na direção do som. Desde que todo esse incidente bizarro aconteceu, essas foram as primeiras vozes que ouviu.</p>  
+
+<p>O jovem pálido logo avistou uma das fontes. Um homem musculoso, moreno e com cabelo bem curto, vinha em sua direção. Ele usava uma espécie de armadura, que tinha toques medievais, mas, ao mesmo tempo, havia características modernas. Aparentemente era feita de um material que parecia ser um metal negro. Um grande brasão dourado, com o formato que se assemelha a uma cabeça de leão, estava estampado no lado direito de seu peitoral.</p>  
+
+<p>Apesar de ser uma armadura, não era igual às famosas armaduras medievais que limitavam a mobilidade, pelo contrário, parecia bem confortável. Em sua cintura, do lado esquerdo, havia algo que parecia ser uma espada que balançava enquanto ele andava em sua direção sobre os espaços entre as pessoas no chão.</p>  
+
+<p>O que é isso? Esse cara está fazendo alguma espécie de cosplay?</p>  
+
+<p>Fernando achou a aparência do homem estranha, mas não se importou muito. Estava feliz em ver alguém totalmente consciente, provavelmente essa pessoa saberia mais a respeito do que estava acontecendo.</p>  
+
+<p>Logo o sujeito chegou até ele.</p>  
+
+<p>“Ei, moleque, consegue se levantar?” O homem moreno de armadura perguntou.</p>  
+
+<p>“Eu…”</p>  
+
+<p>Coff! Coff! Coff!</p>  
+
+<p>Fernando tentou responder, mas aparentemente sua língua e garganta ainda estavam levemente entorpecidas e acabou engasgando.</p>  
+
+<p>“Calma, rapaz. Comece tentando fazer alguns ruídos para exercitar a garganta, depois você tenta falar.” O homem parecia estar acostumado a esse tipo de situação e o instruiu.</p>  
+
+<p>Fernando achou estranho, mas seguiu com o que lhe foi falado. Após alguns testes vocais e ruídos, ele começou a conseguir falar algumas sílabas e depois palavras curtas, todo o processo demorou pouco tempo. O homem assentiu sem se importar muito e perguntou:</p>  
+
+<p>“Você consegue andar?”</p>  
+
+<p>“Eu… acho que não. Talvez se eu tentar um pouco… eu consiga.” O jovem respondeu pausadamente. Logo após, tentou se levantar, e como esperado, suas pernas mal respondiam e tornou a cair sentado.</p>  
+
+<p>Após esticar as pernas um pouco e fazer algum alongamento, finalmente conseguiu se manter de pé e seus movimentos pareciam estar relativamente normais.</p>  
+
+<p>“Bom, agora que você já pode andar, me siga.”</p>  
+
+<p>“É… Se eu puder saber… para onde vamos? E quanto a essas pessoas?” Ele reflexivamente olhou para a ruiva que estava no chão. A garota ainda estava parada, mas parecia que já conseguia movimentar levemente o pescoço. Ela o olhava pelo canto dos olhos no chão.</p>  
+
+<p>“Não se importe com eles. Logo acordarão e serão levados para o mesmo local que você.” O homem disse de forma indiferente.</p>  
+
+<p>Fernando pensou em seguir o sujeito, mas ao ver aquele par de olhos verdes cintilantes, se sentiu mal.</p>  
+
+<p>Será que devo ir com esse cara? Ou eu deveria esperar essa garota conseguir se mover? Mas pelo tempo que ela levou para mover sua cabeça, vai levar pelo menos uma hora para movimentar todo o corpo… Com certeza esse bombado não vai querer esperar, talvez ele até se irrite comigo. Não quero provocar um cara tão forte, mas também não me sinto à vontade para deixa-lá largada aqui… Se pelo menos ela não ficasse me encarando, talvez eu não me importasse tanto… Droga! Por que sou tão molenga?</p>  
+
+<p>“Err… Com licença, será que… Eu posso levá-la comigo?” disse de forma hesitante, enquanto apontava para a garota ruiva no chão.</p>  
+
+<p>O homem olhou na direção que Fernando apontou e percebeu a garota, a inspecionando. Logo, alguma realização o atingiu, então ele fez uma expressão de desgosto.</p>  
+
+<p>“Pirralho, o que você está pensando em fazer? Você acha mesmo que eu vou deixar você cometer atos imorais na minha frente? Deixe-me avisá-lo, é melhor você esquecer esses pensamentos ou então não terá uma vida agradável aqui.” O sujeito falou, enquanto levava a mão esquerda a cintura e roçava fracamente em torno do punho da espada, ele parecia irritado.</p>  
+
+<p>Porra! Ele me entendeu completamente errado! Tudo que eu não queria era irritar esse cara e foi justamente o que eu fiz!</p>  
+
+<p>“Não é o que você está pensando! Eu apenas… não queria deixá-la aqui… Se de qualquer forma ela vai para o mesmo lugar que eu… então apenas pensei que poderia ajudá-la…” Fernando nervosamente se explicou, ele com certeza percebeu o homem tocar a espada.</p>  
+
+<p>Não sabia se era uma espada real ou apenas uma réplica de plástico, mas independente do que fosse, esse homem só precisava usar as mãos nuas para acabar com ele.</p>  
+
+<p>Apesar de não ser alto, sendo até um pouco menor que Fernando, o sujeito era musculoso e, agora, passava uma sensação ameaçadora.</p>  
+
+<p>Depois da explicação apressada do rapaz a sua frente, a expressão do sujeito moreno se aliviou um pouco, mas um leve vestígio de desconfiança ainda cruzava seus olhos.</p>  
+
+<p>“Se é assim, tudo bem, então. Geralmente não carregamos os recrutas desacordados ou paralisados porque seria muito trabalhoso. Se você está disposto, eu não me importo, mas é melhor não ter segundas intenções ou as coisas vão azedar para o seu lado.”</p>  
+
+<p>“Não, claro que não… Tenha certeza…” respondeu, suor frio escorria de suas costas. Apesar de não ser do tipo fisicamente frágil, ele estava apenas na média de físico de um jovem comum, jamais ganharia uma luta contra um monstro de academia como esse.</p>  
+
+<p>Além disso, mesmo que ambos tivessem a mesma estrutura física, não necessariamente ele sairia vitorioso, pois nunca havia brigado na vida. Sempre que as pessoas o intimidavam na infância, Fernando geralmente cedia e não reagia. Graças a isso, os valentões da escola acabaram por perder o interesse nele.</p>  
+
+<p>Apesar da situação ruim que acabou de passar, ao menos teve um lado positivo, ele conseguiu ouvir algumas informações. Aparentemente, tudo que estava acontecendo era normal para esse cara.</p>  
+
+<p>Mas afinal, qual é a ligação entre aquelas luzes coloridas e esse lugar? E o que ele quis dizer com recrutas? Fernando ponderou secretamente. Com certeza, se lembrava de ter ouvido esse termo na primeira vez que ouviu a voz do homem.</p>
   `;
   
   chapterText.innerHTML = chapterContent;
